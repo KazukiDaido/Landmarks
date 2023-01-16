14e4cf3207b5a602ca4b16799ee0f7cdfac801a1
KazukiDaido <k10de302222@gmail.com>
2023-01-17 02:48:38 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 大道一輝 on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
