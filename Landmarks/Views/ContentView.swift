364f4c7c206eee2fd4022ac33cb1fb9501e851bf
KazukiDaido <k10de302222@gmail.com>
2023-01-26 17:49:57 +0900

    コミット漏れを修正

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,42 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130.0)
-            
-            VStack(alignment: .leading) {
-                Text("Tuetle Rock")
-                    .font(.title)
-                HStack {
-                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+                .environmentObject(ModelData())
         }
     }
